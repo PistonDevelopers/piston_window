0bb8834e1e019dd2cf3cef1c0912547d9d1f0223
Sven Nilsen <bvssvni@gmail.com>
2015-04-14 03:21:23 +0200

    Added Gfx and Gfx2d setup

--- examples/hello_piston.rs
+++ examples/hello_piston.rs
@@ -1,6 +1,7 @@
 extern crate piston_window;
 extern crate glutin_window;
 extern crate piston;
+extern crate graphics;
 
 use std::cell::RefCell;
 use std::rc::Rc;
@@ -8,24 +9,31 @@ use glutin_window::{ OpenGL, GlutinWindow };
 use piston_window::*;
 use piston::window::{ WindowSettings, Size };
 
+use piston::event::*;
+use graphics::*;
+use piston::input::*;
+
 fn main() {
     let window = Rc::new(RefCell::new(GlutinWindow::new(
         OpenGL::_3_2,
-        WindowSettings::new("Loading...".to_string(),
+        WindowSettings::new("Hello Piston!".to_string(),
             Size { width: 640, height: 480 })
             .exit_on_esc(true)
     )));
-    for mut e in PistonWindow::new(window, empty_app()) {
-        use piston::event::*;
-        use piston::window::*;
+    println!("Press a button to enter inner loop");
+    for e in PistonWindow::new(window, empty_app()) {
+        e.draw_2d(|_c, g| {
+            clear([0.5, 1.0, 0.5, 1.0], g);
+        });
 
-        e.set_title("Hello Piston!".to_string());
         if let Some(button) = e.press_args() {
-            use piston::input::{ Button, Key };
-
             println!("Pressed {:?}", button);
             println!("Press X to exit inner loop");
-            for e in e {
+            for e in e.clone() {
+                e.draw_2d(|_c, g| {
+                    clear([0.5, 0.5, 1.0, 1.0], g);
+                });
+
                 // Inner loop.
                 if let Some(Button::Keyboard(Key::X)) = e.press_args() {
                     break;
