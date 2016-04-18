9c9ae722c61751f75d978c64c29e3db7e73c6cef
Marcel Müller <neikos@neikos.email>
2016-04-18 14:04:37 +0200

    Fix doc examples and make them build

--- src/lib.rs
+++ src/lib.rs
@@ -15,17 +15,18 @@
 //!
 //! ### Example
 //!
-//! ```ignore
+//! ```no_run
 //! extern crate piston_window;
 //!
 //! use piston_window::*;
 //!
 //! fn main() {
-//!     let window: PistonWindow =
+//!     let mut window: PistonWindow =
 //!         WindowSettings::new("Hello World!", [512; 2])
 //!             .build().unwrap();
-//!     for e in window {
-//!         e.draw_2d(|c, g| {
+//!     let mut events = window.events();
+//!     while let Some(e) = events.next(&mut window) {
+//!         window.draw_2d(&e, |c, g| {
 //!             clear([0.5, 0.5, 0.5, 1.0], g);
 //!             rectangle([1.0, 0.0, 0.0, 1.0], // red
 //!                       [0.0, 0.0, 100.0, 100.0], // rectangle
@@ -39,10 +40,18 @@
 //!
 //! Change the second generic parameter to the window back-end you want to use.
 //!
-//! ```ignore
-//! let window: PistonWindow<(), Sdl2Window> =
+//! ```no_run
+//! extern crate piston_window;
+//!
+//! use piston_window::*;
+//!
+//! # fn main() {
+//!
+//! let window: PistonWindow =
 //!     WindowSettings::new("title", [512; 2])
 //!         .build().unwrap();
+//!
+//! # }
 //! ```
 //!
 //! ### sRGB
