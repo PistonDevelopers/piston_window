9badcfb13cee00abbc996d00ed8eeee171608bc3
Atul Bhosale <atul1bhosale@gmail.com>
2019-12-20 00:09:34 +0530

    Format code using 'cargo fmt'

--- examples/hello_piston.rs
+++ examples/hello_piston.rs
@@ -7,20 +7,26 @@ fn main() {
     let mut window: PistonWindow = WindowSettings::new(title, [640, 480])
         .exit_on_esc(true)
         .build()
-        .unwrap_or_else(|e| { panic!("Failed to build PistonWindow: {}", e) });
+        .unwrap_or_else(|e| panic!("Failed to build PistonWindow: {}", e));
 
     window.set_lazy(true);
     while let Some(e) = window.next() {
         window.draw_2d(&e, |c, g, _| {
             clear([0.5, 1.0, 0.5, 1.0], g);
-            rectangle([1.0, 0.0, 0.0, 1.0], [50.0, 50.0, 100.0, 100.0], c.transform, g);
+            rectangle(
+                [1.0, 0.0, 0.0, 1.0],
+                [50.0, 50.0, 100.0, 100.0],
+                c.transform,
+                g,
+            );
         });
 
         if e.press_args().is_some() {
             InnerApp {
                 title: "Inner loop (press X to exit inner loop)",
                 exit_button: Button::Keyboard(Key::X),
-            }.run(&mut window);
+            }
+            .run(&mut window);
             window.set_title(title.into());
         }
     }
@@ -38,7 +44,12 @@ impl InnerApp {
         while let Some(e) = window.next() {
             window.draw_2d(&e, |c, g, _| {
                 clear([0.5, 0.5, 1.0, 1.0], g);
-                ellipse([1.0, 0.0, 0.0, 1.0], [50.0, 50.0, 100.0, 100.0], c.transform, g);
+                ellipse(
+                    [1.0, 0.0, 0.0, 1.0],
+                    [50.0, 50.0, 100.0, 100.0],
+                    c.transform,
+                    g,
+                );
             });
             if let Some(button) = e.press_args() {
                 if button == self.exit_button {
