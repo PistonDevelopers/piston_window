44c8abb502c58d2c1fb9c77ece84838012e6b0dd
Sven Nilsen <bvssvni@gmail.com>
2025-12-05 21:18:21 +0100

    Transition to Winit and WGPU, batteries included
    See https://github.com/PistonDevelopers/piston_window/issues/317

--- src/lib.rs
+++ src/lib.rs
@@ -1,93 +1,5 @@
 #![deny(missing_docs)]
-
-//! The official Piston window wrapper for the Piston game engine
-//!
-//! **Notice! If this is your first time visiting Piston, [start here](https://github.com/PistonDevelopers/piston).**
-//!
-//! The purpose of this library is to provide an easy-to-use,
-//! simple-to-get-started and convenient-for-applications API for Piston.
-//!
-//! Sets up:
-//!
-//! - [Gfx](https://github.com/gfx-rs/gfx) with an OpenGL back-end.
-//! - [gfx_graphics](https://github.com/pistondevelopers/gfx_graphics)
-//! for 2D rendering.
-//! - [glutin_window](https://github.com/pistondevelopers/glutin_window)
-//! as default window back-end, but this can be swapped (see below).
-//!
-//! ### Example
-//!
-//! ```no_run
-//! extern crate piston_window;
-//!
-//! use piston_window::*;
-//!
-//! fn main() {
-//!     let mut window: PistonWindow =
-//!         WindowSettings::new("Hello World!", [512; 2])
-//!             .build().unwrap();
-//!     while let Some(e) = window.next() {
-//!         window.draw_2d(&e, |c, g, _| {
-//!             clear([0.5, 0.5, 0.5, 1.0], g);
-//!             rectangle([1.0, 0.0, 0.0, 1.0], // red
-//!                       [0.0, 0.0, 100.0, 100.0], // rectangle
-//!                       c.transform, g);
-//!         });
-//!     }
-//! }
-//! ```
-//!
-//! The `draw_2d` function calls the closure on render events.
-//! There is no need to filter events manually, and there is no overhead.
-//!
-//! ### Swap to another window back-end
-//!
-//! Change the generic parameter to the window back-end you want to use.
-//!
-//! ```ignore
-//! extern crate piston_window;
-//! extern crate sdl2_window;
-//!
-//! use piston_window::*;
-//! use sdl2_window::Sdl2Window;
-//!
-//! # fn main() {
-//!
-//! let window: PistonWindow<Sdl2Window> =
-//!     WindowSettings::new("title", [512; 2])
-//!         .build().unwrap();
-//!
-//! # }
-//! ```
-//!
-//! ### sRGB
-//!
-//! The impl of `BuildFromWindowSettings` in this library turns on
-//! `WindowSettings::srgb`, because it is required by gfx_graphics.
-//!
-//! Most images such as those found on the internet uses sRGB,
-//! that has a non-linear gamma corrected space.
-//! When rendering 3D, make sure textures and colors are in linear gamma space.
-//! Alternative is to use `Srgb8` and `Srgba8` formats for textures.
-//!
-//! For more information about sRGB, see
-//! https://github.com/PistonDevelopers/piston/issues/1014
-//!
-//! ### Library dependencies
-//!
-//! This library is meant to be used in applications only.
-//! It is not meant to be depended on by generic libraries.
-//! Instead, libraries should depend on the lower abstractions,
-//! such as the [Piston core](https://github.com/pistondevelopers/piston).
-
-extern crate gfx;
-extern crate gfx_device_gl;
-extern crate gfx_graphics;
-extern crate piston;
-extern crate shader_version;
-
-pub extern crate graphics;
-pub extern crate texture;
+#![doc = include_str!("../README.md")]
 
 /// Exports all of the types exposed by this module, *except* for `graphics`.
 ///
@@ -97,5 +9,46 @@ pub extern crate texture;
 /// Using the name "prelude" also suppresses the wildcard import warning from clippy.
 pub mod prelude;
 
-pub use graphics::*;
+pub mod piston_window;
+
 pub use prelude::*;
+
+pub use graphics;
+pub use piston;
+pub use texture;
+pub use wgpu;
+pub use wgpu_graphics;
+
+// Reexport commonly used libraries.
+#[cfg(feature = "batteries")]
+pub use ai_behavior;
+#[cfg(feature = "batteries")]
+pub use bevy;
+#[cfg(feature = "batteries")]
+pub use camera_controllers;
+#[cfg(feature = "batteries")]
+pub use collada;
+#[cfg(feature = "batteries")]
+pub use dyon;
+#[cfg(feature = "batteries")]
+pub use gltf;
+#[cfg(feature = "batteries")]
+pub use image;
+#[cfg(feature = "batteries")]
+pub use kira;
+#[cfg(feature = "batteries")]
+pub use nalgebra;
+#[cfg(feature = "batteries")]
+pub use piston_meta;
+#[cfg(feature = "batteries")]
+pub use sprite;
+#[cfg(feature = "batteries")]
+pub use rapier2d;
+#[cfg(feature = "batteries")]
+pub use rapier3d;
+#[cfg(feature = "batteries")]
+pub use texture_packer;
+#[cfg(feature = "batteries")]
+pub use vecmath;
+#[cfg(feature = "batteries")]
+pub use wavefront_obj;
---
+++ src/piston_window.rs
@@ -0,0 +1,277 @@
+//! Piston Window module.
+
+use piston::{
+    AdvancedWindow,
+    BuildFromWindowSettings,
+    Event,
+    Events,
+    EventLoop,
+    EventSettings,
+    GenericEvent,
+    Position,
+    ResizeArgs,
+    Size,
+    Window,
+    WindowSettings,
+};
+use graphics::{Context};
+
+use wgpu_graphics::{TextureContext, Wgpu2d, WgpuGraphics};
+use std::error::Error;
+use std::time::Duration;
+use std::sync::Arc;
+
+/// Glyph cache.
+pub type Glyphs<'a> = wgpu_graphics::GlyphCache<'a>;
+/// Type for 2D graphics.
+pub type G2d = wgpu_graphics::Wgpu2d;
+/// Texture type compatible with `G2d`.
+pub type G2dTexture = wgpu_graphics::Texture;
+
+use winit_window::WinitWindow;
+/// Contains everything required for controlling window, graphics, event loop.
+pub struct PistonWindow {
+    /// The window.
+    pub window: WinitWindow,
+    /// WGPU device.
+    pub device: Arc<wgpu::Device>,
+    /// WGPU Command buffer queue.
+    pub queue: wgpu::Queue,
+    /// WGPU surface.
+    pub surface: wgpu::Surface<'static>,
+    /// WGPU surface config.
+    pub surface_config: wgpu::SurfaceConfiguration,
+    /// Wgpu2d.
+    pub g2d: Wgpu2d,
+    /// Event loop state.
+    pub events: Events,
+}
+
+impl BuildFromWindowSettings for PistonWindow {
+    fn build_from_window_settings(
+        settings: &WindowSettings,
+    ) -> Result<Self, Box<dyn Error>> {
+        // Turn on sRGB.
+        let settings = settings.clone().srgb(true);
+        Ok(PistonWindow::new(settings.build()?))
+    }
+}
+
+impl PistonWindow {
+    /// Creates a new Piston window.
+    pub fn new(window: WinitWindow) -> Self {
+        use wgpu::{PresentMode, SurfaceConfiguration, TextureFormat};
+
+        fn init_surface_config(window: &WinitWindow) -> SurfaceConfiguration {
+            SurfaceConfiguration {
+                usage: wgpu::TextureUsages::RENDER_ATTACHMENT,
+                format: TextureFormat::Bgra8UnormSrgb,
+                width: window.draw_size().width as u32,
+                height: window.draw_size().height as u32,
+                present_mode: PresentMode::Fifo,
+                alpha_mode: wgpu::CompositeAlphaMode::PostMultiplied,
+                view_formats: vec![TextureFormat::Bgra8UnormSrgb],
+                desired_maximum_frame_latency: Default::default(),
+            }
+        }
+
+        let instance = wgpu::Instance::new(&Default::default());
+        let surface = instance.create_surface(window.get_window()).unwrap();
+        let adapter =
+            futures::executor::block_on(instance.request_adapter(&wgpu::RequestAdapterOptions {
+                compatible_surface: Some(&surface),
+                ..Default::default()
+            })).unwrap();
+
+        let mut device_descriptor = wgpu::DeviceDescriptor::default();
+        device_descriptor.required_features.set(wgpu::Features::DEPTH_CLIP_CONTROL, true);
+        let (device, queue) = futures::executor::block_on(
+            adapter.request_device(&device_descriptor),
+        ).unwrap();
+        let surface_config = init_surface_config(&window);
+        surface.configure(&device, &surface_config);
+        let device = Arc::new(device);
+        let g2d = Wgpu2d::new(device.clone(), &surface_config);
+        let events = Events::new(EventSettings::new());
+        PistonWindow {
+            window,
+            events,
+            device,
+            surface,
+            surface_config,
+            queue,
+            g2d,
+        }
+    }
+}
+
+impl PistonWindow {
+    /// Creates context used to create and update textures.
+    pub fn create_texture_context<'a>(&'a self) -> TextureContext<'a> {
+        TextureContext::from_parts(&self.device, &self.queue)
+    }
+
+    /// Loads font from a path.
+    pub fn load_font<'a, P: AsRef<std::path::Path>>(
+        &'a self,
+        path: P,
+    ) -> Result<Glyphs<'a>, std::io::Error> {
+        Glyphs::new(
+            path,
+            self.create_texture_context(),
+            texture::TextureSettings::new(),
+        )
+    }
+
+    /// Renders 2D graphics.
+    ///
+    /// Calls the closure on render events.
+    /// There is no need to filter events manually, and there is no overhead.
+    pub fn draw_2d<E, F, U>(&mut self, e: &E, f: F) -> Option<U>
+    where
+        E: GenericEvent,
+        F: FnOnce(Context, &mut WgpuGraphics, &wgpu::Device) -> U,
+    {
+        if let Some(args) = e.render_args() {
+            let surface_texture = self.surface.get_current_texture().unwrap();
+            let surface_view = surface_texture.texture
+                .create_view(&wgpu::TextureViewDescriptor::default());
+
+            let device = &self.device;
+            let (res, command_buffer) = self.g2d.draw(
+                &self.surface_config,
+                &surface_view,
+                args.viewport(),
+                |c, g| f(c, g, device));
+            self.queue.submit(std::iter::once(command_buffer));
+            surface_texture.present();
+            Some(res)
+        } else {
+            None
+        }
+    }
+
+    /// Renders 3D graphics.
+    ///
+    /// Calls the closure on render events.
+    /// There is no need to filter events manually, and there is no overhead.
+    pub fn draw_3d<E, F, U>(&mut self, e: &E, f: F) -> Option<U>
+    where
+        E: GenericEvent,
+        F: FnOnce(&mut Self) -> U,
+    {
+        if let Some(_) = e.render_args() {Some(f(self))} else {None}
+    }
+
+    /// Let window handle new event.
+    /// Cleans up after rendering and resizes frame buffers.
+    pub fn event<E: GenericEvent>(&mut self, e: &E) {
+        e.resize(
+            |&ResizeArgs {
+                 draw_size: [width, height],
+                 ..
+             }| {
+                self.surface_config = wgpu::SurfaceConfiguration {
+                        width,
+                        height,
+                        ..self.surface_config.clone()
+                    };
+                    self.surface.configure(&self.device, &self.surface_config);
+            },
+        );
+    }
+}
+
+impl Window for PistonWindow {
+    fn should_close(&self) -> bool {
+        self.window.should_close()
+    }
+    fn set_should_close(&mut self, value: bool) {
+        self.window.set_should_close(value)
+    }
+    fn size(&self) -> Size {
+        self.window.size()
+    }
+    fn draw_size(&self) -> Size {
+        self.window.draw_size()
+    }
+    fn swap_buffers(&mut self) {
+        // Wait for queued commends to finish,
+        // so they get included in the frame render.
+        let _ = self.device.poll(wgpu::PollType::wait_indefinitely());
+        self.window.swap_buffers()
+    }
+    fn wait_event(&mut self) -> Event {
+        Window::wait_event(&mut self.window)
+    }
+    fn wait_event_timeout(&mut self, timeout: Duration) -> Option<Event> {
+        Window::wait_event_timeout(&mut self.window, timeout)
+    }
+    fn poll_event(&mut self) -> Option<Event> {
+        Window::poll_event(&mut self.window)
+    }
+}
+
+impl AdvancedWindow for PistonWindow {
+    fn get_title(&self) -> String {
+        self.window.get_title()
+    }
+    fn set_title(&mut self, title: String) {
+        self.window.set_title(title)
+    }
+    fn get_automatic_close(&self) -> bool {
+        self.window.get_automatic_close()
+    }
+    fn set_automatic_close(&mut self, value: bool) {
+        self.window.set_automatic_close(value);
+    }
+    fn get_exit_on_esc(&self) -> bool {
+        self.window.get_exit_on_esc()
+    }
+    fn set_exit_on_esc(&mut self, value: bool) {
+        self.window.set_exit_on_esc(value)
+    }
+    fn set_capture_cursor(&mut self, value: bool) {
+        self.window.set_capture_cursor(value)
+    }
+    fn show(&mut self) {
+        self.window.show()
+    }
+    fn hide(&mut self) {
+        self.window.hide()
+    }
+    fn get_position(&self) -> Option<Position> {
+        self.window.get_position()
+    }
+    fn set_position<P: Into<Position>>(&mut self, pos: P) {
+        self.window.set_position(pos)
+    }
+    fn set_size<S: Into<Size>>(&mut self, size: S) {
+        self.window.set_size(size)
+    }
+}
+
+impl EventLoop for PistonWindow {
+    fn get_event_settings(&self) -> EventSettings {
+        self.events.get_event_settings()
+    }
+
+    fn set_event_settings(&mut self, settings: EventSettings) {
+        self.events.set_event_settings(settings);
+    }
+}
+
+impl Iterator for PistonWindow {
+    type Item = Event;
+
+    /// Returns next event.
+    /// Cleans up after rendering and resizes frame buffers.
+    fn next(&mut self) -> Option<Self::Item> {
+        if let Some(e) = self.events.next(&mut self.window) {
+            self.event(&e);
+            Some(e)
+        } else {
+            None
+        }
+    }
+}
